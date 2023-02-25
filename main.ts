3d59bf7143f0f1c2b26e25550a7b0f6f2066cdd2
erikguy <126192570+erikguy@users.noreply.github.com>
2023-02-24 21:50:58 -0700

    fixed title screen

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 let mySprite: Sprite = null
+let Game_Scene = "Title Screen"
+Render.setViewMode(ViewMode.tilemapView)
 scene.setBackgroundImage(assets.image`title screen`)
 music.play(music.createSong(hex`0078000408040106001c00010a006400f4016400000400000000000000000000000000000000021c0000002000020c1120004000020d1240006000020c1160008000020a0f`), music.PlaybackMode.LoopingInBackground)
-let Game_Scene = "Title Screen"
 /**
  * Hallway
  */
