72622d89368178343a3ba71444403ad3506618cd
erikguy <126192570+erikguy@users.noreply.github.com>
2023-02-25 13:02:47 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,90 @@
-let mySprite = Render.getRenderSpriteVariable()
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (cams == 0 && mySprite.overlapsWith(Cameras)) {
+        Render.setViewMode(ViewMode.tilemapView)
+        mySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        mySprite.setPosition(150, 190)
+        cams = 1
+    } else {
+        if (cams == 1) {
+            mySprite.setPosition(150, 300)
+            cams = 2
+        } else {
+            if (cams == 2) {
+                mySprite.setPosition(26, 400)
+                cams = 3
+            } else {
+                if (cams == 3) {
+                    Render.setViewMode(ViewMode.raycastingView)
+                    mySprite.setImage(img`
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                        `)
+                    mySprite = Render.getRenderSpriteVariable()
+                    cams = 0
+                    mySprite.setPosition(58, 29)
+                }
+            }
+        }
+    }
+})
+let Cameras: Sprite = null
+let cams = 0
+let mySprite: Sprite = null
+mySprite = Render.getRenderSpriteVariable()
+cams = 0
+Cameras = sprites.create(img`
+    . . . b b b b b b b b b . . . . 
+    . . b 1 d d d d d d d 1 b . . . 
+    . b 1 1 1 1 1 1 1 1 1 1 1 b . . 
+    . b d b c c c c c c c b d b . . 
+    . b d c 6 6 6 6 6 6 6 c d b . . 
+    . b d c 6 d 6 6 6 6 6 c d b . . 
+    . b d c 6 6 6 6 6 6 6 c d b . . 
+    . b d c 6 6 6 6 6 6 6 c d b . . 
+    . b d c 6 6 6 6 6 6 6 c d b . . 
+    . b d c c c c c c c c c d b . . 
+    . c b b b b b b b b b b b c . . 
+    c b c c c c c c c c c c c b c . 
+    c 1 d d d d d d d d d d d 1 c . 
+    c 1 d 1 1 d 1 1 d 1 1 d 1 1 c . 
+    c b b b b b b b b b b b b b c . 
+    c c c c c c c c c c c c c c c . 
+    `, SpriteKind.Player)
+Cameras.setPosition(58, 29)
 Render.toggleViewMode()
 Render.setViewMode(ViewMode.raycastingView)
-Render.setAttribute(Render.attribute.wallZScale, 12)
+Render.setAttribute(Render.attribute.wallZScale, 15)
 scene.setBackgroundImage(img`
     cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
     cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
@@ -126,3 +209,6 @@ scene.setBackgroundImage(img`
     `)
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.setBackgroundColor(13)
+forever(function () {
+    music.play(music.createSong(hex`0078000408060203001c0001dc00690000045e01000400000000000000000000056400010400031e006800700001197000780001207c008400011d84008c0001208c009400011d04001c00100500640000041e000004000000000000000000000000000a040004420000000c0001050c001800010618002400010824003000010530003400010834003c0001063c004400010844005400010a540064000105640074000108740084000105`), music.PlaybackMode.UntilDone)
+})
